d8d8d9ff8766033b52c890ca6cdf196c8ebc5af9
周重淳 <2018010223@secoder.net>
2020-10-20 11:07:48 +0800

    Finish a Multi-port subckt generator design

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class Main{
     public static void main(String args[]) {
-        String s = "C? nodei? nodeo? 50p";
-        IntelligentLineDuplicater(s, 200);
+        IntelligentLineDuplicater("C? nodei? nodeo? 50p", 200);
+        MultiPortSubcktGenerator("name", "nodei1 nodei2?node102 nodei3");
     }
     public static void IntelligentLineDuplicater(String s, int num) {
         for(int lptr = 0; lptr < num; lptr++){
@@ -17,5 +17,32 @@ public class Main{
             }
             System.out.println(str);
         }
+    }
+    public static void MultiPortSubcktGenerator(String name, String portList) {
+        String titLine = ".subckt " + name + " ";
+        String endLine = ".ends " + name;
+        for(int i = 0; i < portList.length(); i++){
+            if(portList.charAt(i) == '?'){
+                int tagIndex = i;
+                int nameEndIndex = i;
+                int DataEndIndex = i;
+                int j;
+                for(j = i + 1; (portList.charAt(j) >= 'a' && portList.charAt(j) <= 'z') || (portList.charAt(j) >= 'A' && portList.charAt(j) <= 'Z'); j++);
+                nameEndIndex = j;
+                for(;(j < portList.length()) && (portList.charAt(j) >= '0' && portList.charAt(j) <= '9'); j++);
+                DataEndIndex = j;
+                String portName = portList.substring(tagIndex + 1, nameEndIndex);
+                int num = Integer.parseInt(portList.substring(nameEndIndex, DataEndIndex));
+                String s1 = portList.substring(0, tagIndex);
+                String s2 = portList.substring(DataEndIndex);
+                for(int portPtr = 0; portPtr < num; portPtr++){
+                    s1 = s1 + " " + portName + portPtr;
+                }
+                portList = s1 + s2;
+            }
+        }
+        titLine = titLine + portList;
+        System.out.println(titLine);
+        System.out.println(endLine);
     }
 }
